aad2fc44bc8c9e077181b33f8d313c4a537edee3
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2024-03-05 21:10:16 +0300

    Initial Commit

---
+++ ColorizedAppV2/ColorizedAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppV2App.swift
+//  ColorizedAppV2
+//
+//  Created by Rustam Cherezbiev on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
